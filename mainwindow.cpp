507a3b10731983e0f977bc261da02b4422112405
saulgold <saulgold@gmail.com>
2016-07-11 16:49:49 +0100

    skin tone detection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,16 +4,20 @@
 #include<QFileDialog>
 #include<QtCore>
 
-
+cv::Ptr < cv::BackgroundSubtractorMOG2> pMOG2;
+cv::Mat MOGmask;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
+
     if(!m_cap.open(0)){
         qDebug()<<"webcam no open";
     }
 
+    blurSize=1;
     ui->setupUi(this);
     timer = new QTimer(this);
 
@@ -32,16 +36,42 @@ MainWindow::~MainWindow()
 
 
 void MainWindow::updateGUI(){
-  cv::Mat frame;
-  QImage qframe;
+  cv::Mat frame,contourMat,back,skin_frame,min_frame, max_frame;
+  min_frame.setTo(cv::Scalar(0,133,77));
+  max_frame.setTo(cv::Scalar(255,173,127));
 
+
+  QImage qframe;
+  cv::Size blurMask = cv::Size(blurSize,blurSize);
 
   m_cap>>frame;
+  cv::flip(frame,frame,1);
+  cv::cvtColor(frame,frame,CV_BGR2YCrCb);
+  cv::inRange(frame,cv::Scalar(0,133,77),cv::Scalar(255,173,127),skin_frame);
+//  cv::blur(frame,frame,blurMask);
+// // cv::threshold(frame,frame,thresholdSliderValue,255,CV_THRESH_BINARY_INV);
+//  cv::Canny(frame,frame,thresholdSliderValue,thresholdSliderValue);
+//  cv::findContours(frame,contours,hierarchy,CV_RETR_TREE,CV_CHAIN_APPROX_SIMPLE,cv::Point(0,0));
+//  contourMat = cv::Mat::zeros(frame.size(),CV_8UC3);
+
+//  double largest_area = 0;
+//  int largest_contour_index= 0;
+
+//  for(int i=0; i<contours.size();i++){
+//      double area = cv::contourArea(contours[i],false);
+//      if (area>largest_area){
+//          largest_area=area;
+//          largest_contour_index =i;
+//      }
+//  }
 
 
-  qframe = convertOpenCVMatToQtQImage(frame);
+// cv::drawContours(frame,contours,largest_contour_index,cv::Scalar(255,0,50),2,8,hierarchy, 0, cv::Point());
+
+
+  qframe = convertOpenCVMatToQtQImage(skin_frame);
   QPixmap pix = QPixmap::fromImage(qframe);
-  ui->videoScreen->setPixmap(pix.scaled(500, 500, Qt::IgnoreAspectRatio, Qt::FastTransformation));
+  ui->videoScreen->setPixmap(pix);
 }
 
 
@@ -54,4 +84,14 @@ QImage MainWindow::convertOpenCVMatToQtQImage(cv::Mat mat) {
          qDebug() << "in convertOpenCVMatToQtQImage, image was not 1 channel or 3 channel, should never get here";
     }
     return QImage();        // return a blank QImage if the above did not work
+}
+
+void MainWindow::on_thresholdSlider_valueChanged(int value)
+{
+    thresholdSliderValue = value;
+}
+
+void MainWindow::on_blurSlider_valueChanged(int value)
+{
+    blurSize = value;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -7,7 +7,7 @@
 #include<opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include<opencv2/imgproc/imgproc.hpp>
-
+#include<opencv2/video.hpp>
 #include "qdebug.h"
 #include <QImage>
 #include <QtCore>
@@ -15,9 +15,23 @@
 #include<vector>
 #include<algorithm>
 #include "vidcap.h"
+#include "opencv2/opencv.hpp"
+#include <vector>
+#include <stdio.h>
+#include<stdlib.h>
 
 #include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
 
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/features2d/features2d.hpp>
+#include "ml.h"
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <opencv2/video/background_segm.hpp>
 #include <QTime>
 
 
@@ -37,11 +51,20 @@ public:
 private slots:
     void updateGUI();
 
+    void on_thresholdSlider_valueChanged(int value);
+
+    void on_blurSlider_valueChanged(int value);
+
 private:
     Ui::MainWindow *ui;
     QImage MainWindow::convertOpenCVMatToQtQImage(cv::Mat mat);
     QTimer *timer;
       cv::VideoCapture m_cap;
+     int thresholdSliderValue;
+     int blurSize;
+     std::vector<std::vector<cv::Point> > contours;
+     std::vector<cv::Vec4i> hierarchy;
+
     //cv::Ptr<cv::BackgroundSubtractorMOG2> pMOG;
 
 };
